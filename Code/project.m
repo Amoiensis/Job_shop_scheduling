374757b97b154701819f924372cbd2626754ecb0
Amoiensis <51776835+Amoiensis@users.noreply.github.com>
2019-06-29 23:03:57 +0800

    Create project.m

---
+++ Code/project.m
@@ -0,0 +1,13 @@
+%用于解决 Project4：Job shop scheduling
+%文件分为4部分
+% 1. read_data.m     数据导入
+% 2. build_struct.m  数据预处理
+% 3. core.m          可行解计算
+% 4. calculate.m     成本计算
+
+clear;clc;
+
+read_data;
+build_struct;
+core;
+calculate
